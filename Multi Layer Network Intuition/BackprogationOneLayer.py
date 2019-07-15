Started correcting backpropagation multiples layers (not working yet).

Multi Layer Network Intuition/BackprogationOneLayer.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import math
 from sklearn import preprocessing
 
-np.random.seed(420)
+np.random.seed(42)
 
 
 def relu(val):
@@ -69,7 +69,8 @@ for iteration in range(900):
 
         w1 = w1 - learning_rate * current_derivative_w1
 
-    if(iteration % 100 == 0):
+    if(iteration % 100 == 0 or iteration == 0):
+        print('iteration: ', iteration)
         print('cost: ', cost)
         print('y: ', Y[0:5])
         print('y_hat: ', y_hats[0:5])

Multi Layer Network Intuition/BackprogragationFullNetwork.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import math
+from sklearn import preprocessing
 
 np.random.seed(420)
 
@@ -36,7 +37,7 @@ X = dataset[:, 0:8]
 Y = dataset[:, 8]
 
 # normalize X
-X = X / np.linalg.norm(X)
+X = preprocessing.scale(X)
 
 learning_rate = 0.05
 
@@ -58,30 +59,30 @@ for iteration in range(600):
         y_hats.append(current_y_hat)
         current_y = Y[index]
 
-        cost += cost_function(current_y, current_y_hat)
+        cost += cost_function(current_y, current_y_hat) / X.shape[0]
 
         current_y_hat_derivative = cost_derivative(
             current_y, current_y_hat) / X.shape[0]
 
         # W2 update
-        current_derivative_w2 = current_y_hat_derivative * \
-            sigmoid_derivative(product_w2) * product_w2
+        current_derivative_w2_activation = current_y_hat_derivative * \
+            sigmoid_derivative(product_w2)
 
-        current_derivative_w2 = current_derivative_w2 * activation_w1
+        current_derivative_w2 = current_derivative_w2_activation * activation_w1
         current_derivative_w2 = np.array(current_derivative_w2).reshape(4, 1)
+
         w2 = w2 - learning_rate * current_derivative_w2
 
         # W1 update
-        current_derivative_w1 = current_derivative_w2 * \
-            np.array(list(map(sigmoid_derivative, product_w1))) * product_w1
+        current_derivative_w1_activation = current_derivative_w2 * \
+            np.array(list(map(sigmoid_derivative, product_w1))).reshape(4, 1)
 
-        for current_x_index in range(X[index].shape[0]):
-            current_derivative_w1_at_index = current_derivative_w1[1] * \
-                X[index][current_x_index]
+        current_derivative_w1 = np.dot(X[index].reshape(8, 1),
+                                       current_derivative_w1_activation.reshape(1, 4))
 
-            w1[current_x_index] = w1[current_x_index] - \
-                learning_rate * current_derivative_w1_at_index
+        w1 = w1 - learning_rate * current_derivative_w1
 
-    print('cost: ', cost)
-    print('y: ', Y[4])
-    print('y_hat: ', y_hats[4])
+    if(iteration % 100 == 0):
+        print('cost: ', cost)
+        print('y: ', Y[0:5])
+        print('y_hat: ', y_hats[0:5])